00d3246accae0c8a691de688695641173c49c445
AndrewM <enigmatick@rambler.ru>
2022-09-12 22:38:14 +0300

    Config resources.tf

---
+++ resources.tf
@@ -0,0 +1,24 @@
+resource "yandex_compute_instance" "VM" {
+  name = "workmashine"
+
+  resources {
+    cores  = 4
+    memory = 4
+  }
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+      size     = var.size_hd
+    }
+  }
+  network_interface {
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  metadata = {
+    user-data = "${file("~/users.txt")}"
+  }
+}
