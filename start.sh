4def9ac7fab8915d2ff9efd6c168ff6d63e7af76
AndrewBS <phantom@bstal.ru>
2022-09-16 10:35:49 +0300

    Edit ansible roles

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ `ls  distr/webapps/ | wc -l` -gt 0 ]; then
- mv distr/webapps/*.* tomcat/webapps/
+if [ `ls  distr/ | wc -l` -gt 0 ]; then
+ mv distr/*.* tomcat/webapps/
 fi
 tomcat/bin/startup.sh run && tail -f tomcat/logs/catalina.out
